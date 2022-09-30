363dcd3947b7120a2b92a5759129befbb3400c6f
42Bastian <bs42@monlynx.de>
2022-09-30 13:41:38 +0100

    fix indenting

--- exp/depacker/unlz4.js
+++ exp/depacker/unlz4.js
@@ -17,75 +17,75 @@
 ;;; r13       : end of packed data
 
 depack_lz4::
-		move	R20,R13
-		add	R0,R13			; packed buffer end
-		moveq	#$F,R4
-		movei	#.dpklz4_lenOffset,R10
-		movei	#.dpklz4_tokenLoop,R11
-		movei	#$FF,R12
+	move	R20,R13
+	add	R0,R13		; packed buffer end
+	moveq	#$F,R4
+	movei	#.dpklz4_lenOffset,R10
+	movei	#.dpklz4_tokenLoop,R11
+	movei	#$FF,R12
 
-		loadb	(R20),R0
+	loadb	(R20),R0
 .dpklz4_tokenLoop:
-		addqt	#1,R20
-		move	R0,R1
-		shrq	#4,R1
-		jump	eq,(R10)
-		and	r4,r0			; remove high nibble
+	addqt	#1,R20
+	move	R0,R1
+	shrq	#4,R1
+	jump	eq,(R10)
+	and	r4,r0		; remove high nibble
 
 .dpklz4_readLen1:
-		cmp	R1,R4			; r1 == 15 ?
-		loadb	(R20),R2
-		jr	ne,.dpklz4_readEnd1a ; skip first addq in copy loop!
+	cmp	R1,R4		; r1 == 15 ?
+	loadb	(R20),R2
+	jr	ne,.dpklz4_readEnd1a ; skip first addq in copy loop!
 .dpklz4_readLoop1:
-		addqt	#1,R20
-		add	R2,R1			; final len could be > 64KiB
-		cmp	R12,R2			; r2 = $ff ?
-		jr	eq,.dpklz4_readLoop1
-		loadb	(R20),R2
+	addqt	#1,R20
+	add	R2,R1		; final len could be > 64KiB
+	cmp	R12,R2		; r2 = $ff ?
+	jr	eq,.dpklz4_readLoop1
+	loadb	(R20),R2
 .dpklz4_readEnd1:
 
 .dpklz4_litcopy:
-		addqt	#1,R20
+	addqt	#1,R20
 .dpklz4_readEnd1a:
-		subq	#1,R1
-		storeb	R2,(R21)
-		addqt	#1,R21
-		jr	ne,.dpklz4_litcopy
-		loadb	(R20),r2
+	subq	#1,R1
+	storeb	R2,(R21)
+	addqt	#1,R21
+	jr	ne,.dpklz4_litcopy
+	loadb	(R20),r2
 
-		; end test is always done just after literals
-		cmp	R20,R13
-		jump	eq,(r30)	; done? => return
+	; end test is always done just after literals
+	cmp	R20,R13
+	jump	eq,(r30)	; done? => return
 .dpklz4_lenOffset:
-		loadb	(R20),R1	; read 16bits offset, little endian, unaligned
-		addqt	#1,R20
-		loadb	(R20),R2
-		addqt	#1,R20
-		shlq	#8,R2
-		add	R2,R1
-		neg	r1
-		add	r21,r1		; source = dest - offset
+	loadb	(R20),R1	; read 16bits offset, little endian, unaligned
+	addqt	#1,R20
+	loadb	(R20),R2
+	addqt	#1,R20
+	shlq	#8,R2
+	add	R2,R1
+	neg	r1
+	add	r21,r1	; source = dest - offset
 
 ;;;readLen_smallest2_DSP
-		cmp	R0,R4		; r0 == 15 ?
-		jr	ne,.dpklz4_readEnd2
+	cmp	R0,R4		; r0 == 15 ?
+	jr	ne,.dpklz4_readEnd2
 .dpklz4_readLoop2:
-		loadb	(R20),R2
-		add	R2,R0		; final len could be > 64KiB
-		cmp	R12,R2		; r2 = $ff ?
-		jr	eq,.dpklz4_readLoop2
-		addq	#1,R20
+	loadb	(R20),R2
+	add	R2,R0		; final len could be > 64KiB
+	cmp	R12,R2		; r2 = $ff ?
+	jr	eq,.dpklz4_readLoop2
+	addq	#1,R20
 
 .dpklz4_readEnd2:
 
-		addq	#4,R0
+	addq	#4,R0
 .dpklz4_copy:
-		loadb	(R1),R2
-		addq	#1,R1
-		subq	#1,R0
-		storeb	R2,(R21)
-		jr	ne,.dpklz4_copy
-		addqt	#1,R21
+	loadb	(R1),R2
+	addq	#1,R1
+	subq	#1,R0
+	storeb	R2,(R21)
+	jr	ne,.dpklz4_copy
+	addqt	#1,R21
 
-		jump	(R11)
-		loadb	(R20),R0
+	jump	(R11)
+	loadb	(R20),R0
