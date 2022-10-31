04a6cd7f6276147e14c6167bb233a172751fba4d
42Bastian <bs42@monlynx.de>
2022-10-31 16:51:15 +0100

    depacker:unupkr: down to 1370ms

--- exp/depacker/unupkr.js
+++ exp/depacker/unupkr.js
@@ -8,12 +8,11 @@
 ;;; Register usage (destroyed!)
 ;;; r0-r13,r20,r21
 ;;;
-;;; pack with classic (V1) format.
 
 DST		REG 21
 SRC		REG 20
 
-	REGTOP 15
+	REGTOP 17
 LR_save		REG 99
 LR_save2	REG 99
 GETBIT		REG 99
@@ -27,6 +26,8 @@ prev_was_match	REG 99
 offset		REG 99
 prob		reg 99
 byte		REG 99
+ndata		reg 99
+PROBS		reg 3
 tmp2		reg 2
 tmp1		REG 1
 tmp0		REG 0
@@ -45,21 +46,25 @@ unupkr::
 	jr	pl,.init
 	addq	#4,tmp1
 
+	loadb	(SRC),ndata
+	addq	#1,SRC
 	moveq	#0,offset
 	moveq	#0,state
 	moveq	#0,prev_was_match
 	movei	#.literal,LITERAL
 	movei	#getlength,GETLENGTH
 	movei	#getbit,GETBIT
+	movei	#upkr_probs,PROBS
 .loop
 	move	pc,LOOP
-	moveq	#0,r0
+	moveq	#0,index
 	BL	(GETBIT)
 	jump	cc,(LITERAL)
 	moveq	#0,r0
 	cmpq	#1,prev_was_match
 	jr	eq,.newoff
 	bset	#8,r0
+	move	r0,index
 	BL	(GETBIT)
 	jr	cc,.oldoff
 	shlq	#8,r0
@@ -75,12 +80,32 @@ unupkr::
 
 	move	DST,r1
 	sub	offset,r1
-.cpymatch
+
+	btst	#0,r1
+	jr	ne,.cpymatch1
+	btst	#0,DST
+	jr	ne,.cpymatch1
+	nop
+.cpymatch2
+	loadw	(r1),r2
+	addqt	#2,r1
+	storew	r2,(DST)
+	subq	#2,r0
+	addqt	#2,DST
+	jump	eq,(LOOP)
+	moveq	#1,prev_was_match
+	jr	pl,.cpymatch2
+	nop
+	subq	#1,DST
+	jump	(LOOP)
+	moveq	#1,prev_was_match
+
+.cpymatch1
 	loadb	(r1),r2
 	subq	#1,r0
 	addqt	#1,r1
 	storeb	r2,(DST)
-	jr	ne,.cpymatch
+	jr	ne,.cpymatch1
 	addq	#1,DST
 
 	jump	(LOOP)
@@ -90,14 +115,14 @@ unupkr::
 
 .literal
 	moveq	#1,byte
-	move	byte,r0
+	move	byte,index
 .getbit
 	BL	(GETBIT)
 	addc	byte ,byte
 
 	btst	#8,byte
 	jr	eq,.getbit
-	move	byte,r0
+	move	byte,index
 
 	storeb	byte,(DST)
 	addq	#1,DST
@@ -111,11 +136,9 @@ getlength:
 	move	r0,index
 	moveq	#0,bit_pos
 .gl
-	move	index,r0
 	BL	(GETBIT)
 	jr	cc,.exit
 	addq	#1,index
-	move	index,r0
 	BL	(GETBIT)
 	sh	bit_pos,r0
 	subq	#1,bit_pos
@@ -129,30 +152,31 @@ getlength:
 	or	byte,r0
 
 .newbyte:
-	loadb	(SRC),r1
+	move	ndata,r2
+	loadb	(SRC),ndata
 	shlq	#8,state
 	addq	#1,SRC
-	or	r1,state
+	or	r2,state
 getbit
-	move	state,r1
-	shrq	#12,r1
+	move	PROBS,r1
+	move	state,r2
+	add	index,r1		; r1 = &probs[index]
+	shrq	#12,r2
+	loadb	(r1),prob
 	jr	eq,.newbyte
-	nop
+	moveq	#0,r2
 
-	movei	#upkr_probs,r1
-	add	r1,r0		; r0 = &probs[index]
-	movei	#256,r2
-	loadb	(r0),prob
-	move	state,r1
-	shlq	#24,r1
+	move	state,r0
+	bset	#8,r2
+	shlq	#24,r0
 	shrq	#8,state
-	shrq	#24,r1
-	cmp	prob,r1
+	shrq	#24,r0
+	cmp	prob,r0
 	jr	cs,.one
 	nop
 	sub	prob,r2		; 256-prob
 	mult	state,r2	; (256-prob)*(state>>8)
-	add	r1,r2		; (256-prob)*(state>>8)+(state & 0xff)
+	add	r0,r2		; (256-prob)*(state>>8)+(state & 0xff)
 	sub	prob,r2		; (256-prob)*(state>>8)+(state & 0xff)-prob
 	move	r2,state
 
@@ -160,19 +184,21 @@ getbit
 	add	prob,r2
 	shrq	#4,r2		; (prob+8)>>4
 	sub	r2,prob		; prob - (prob+8)>>4
-	storeb	prob,(r0)
+
 	moveq	#0,r0
+	storeb	prob,(r1)
 	jump	(LR)
 	shrq	#1,r0		; C = 0, r0 = 0
 .one
 	mult	prob,state	; prob*(state>>8)
-	add	r1,state	; prob*(state>>8)+(state & 0xff)
+	add	r0,state	; prob*(state>>8)+(state & 0xff)
 
 	sub	prob,r2		; 256-prob
 	addq	#8,r2
 	shrq	#4,r2
 	add	r2,prob
-	storeb	prob,(r0)
+
 	moveq	#3,r0
+	storeb	prob,(r1)
 	jump	(LR)
 	shrq	#1,r0		; C = 0, r0 = 1
