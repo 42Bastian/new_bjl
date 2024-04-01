4688719c64b1a8e1344adc723c1aa758c400b4a4
42Bastian <bs42@monlynx.de>
2024-04-01 16:56:11 +0200

    unzx0_fast: size down to 192, cleanup

--- exp/depacker/unzx0_fast.js
+++ exp/depacker/unzx0_fast.js
@@ -6,30 +6,29 @@
 ;;; r30 : return address
 ;;;
 ;;; Register usage (destroyed!)
-;;; r0-r13,r20,r21
+;;; r0-r10,r20,r21
 ;;;
 ;;; pack with classic (V1) format.
 
 DST		REG 21
 SRC		REG 20
 
-BYTE_PRELOAD	REG 13
-LR3		REG 12
-LR2		REG 11
-BC		REG 10
-STOR		REG 9
-GETBIT		REG 8
-ELIAS		REG 7
-LITERALS	REG 6
-OFFSET		REG 5
-NEW_OFF		reg 4
-COPY_MATCH	reg 3
-VALUE		REG 2
-tmp1		REG 1
-tmp0		REG 0
+BYTE_PRELOAD	REG 10
+LR3		REG 9
+COPY_TMP	REG 9!
+LR2		REG 8
+DATA_TMP	REG 8!
+STOR		REG 7
+GETBYTE		REG 6
+ELIAS		REG 5
+LITERALS	REG 4
+OFFSET		REG 3
+NEW_OFF		reg 2
+COPY_MATCH	reg 1
+VALUE		REG 0
 
 unzx0::
-	movei	#.getbyte1,GETBIT
+	movei	#.getbyte,GETBYTE
 	movei	#.elias,ELIAS
 	movei	#.copy_match,COPY_MATCH
 	movei	#.new_off,NEW_OFF
@@ -45,56 +44,55 @@ unzx0::
 	jump	(ELIAS)
 	addq	#6,LR2
 .copylit
-	move	BYTE_PRELOAD,r0
+	move	BYTE_PRELOAD,COPY_TMP
 	loadb	(SRC),BYTE_PRELOAD
 	addqt	#1,SRC
 	subq	#1,VALUE
-	storeb	r0,(DST)
+	storeb	COPY_TMP,(DST)
 	jump	ne,(LR2)
 	addqt	#1,DST
 
 	add	STOR,STOR
 	move	PC,LR3
 	jump	ne_cs,(NEW_OFF)
-	addqt	#4,LR3
-	jump	eq,(GETBIT)
-	moveq	#1,VALUE
+	moveq	#0,VALUE
+	jump	eq,(GETBYTE)
+	move	COPY_MATCH,LR2
 	;; last offset
 	jump	(ELIAS)
-	move	COPY_MATCH,LR2
 
 	addqt	#1,VALUE
 .copy_match
-	move	DST,r1
-	sub	OFFSET,r1
+	move	DST,COPY_TMP
+	sub	OFFSET,COPY_TMP
 	btst	#0,DST
 	jr	ne,.copy_match_loop
-	btst	#0,r1
+	btst	#0,COPY_TMP
 	jr	ne,.copy_match_loop
 .copy_match_loop2
-	loadw	(r1),r0
+	loadw	(COPY_TMP),DATA_TMP
 	subq	#2,VALUE
-	storew	r0,(DST)
+	storew	DATA_TMP,(DST)
 	jr	eq,.done
 	addqt	#2,DST
 	jr	pl,.copy_match_loop2
-	addqt	#2,r1
+	addqt	#2,COPY_TMP
 
 	jr	.done
 	subq	#1,DST
 
 .copy_match_loop
-	loadb	(r1),r0
-	addqt	#1,r1
+	loadb	(COPY_TMP),DATA_TMP
+	addqt	#1,COPY_TMP
 	subq	#1,VALUE
-	storeb	r0,(DST)
+	storeb	DATA_TMP,(DST)
 	jr	ne,.copy_match_loop
 	addq	#1,DST
 .done
 	add	STOR,STOR
 	move	PC,LR3
-	jump	eq,(GETBIT)
-	addqt	#6,LR3
+	jump	eq,(GETBYTE)
+	addqt	#6-2,LR3
 
 	jump	cc,(LITERALS)
 .new_off:
@@ -105,41 +103,39 @@ unzx0::
 
 	move	VALUE,OFFSET
 	shlq	#24,VALUE
-	move	BYTE_PRELOAD,r1
+	move	BYTE_PRELOAD,LR3
 	jump	eq,(LR)
 	loadb	(SRC),BYTE_PRELOAD
 	shlq	#7,OFFSET
 	addqt	#1,SRC
-	shrq	#1,r1
+	shrq	#1,LR3
 	move	COPY_MATCH,LR2
 	moveq	#1,VALUE
 	subqt	#2,LR2
 	jump	cs,(LR2)
-	sub	r1,OFFSET
+	sub	LR3,OFFSET
 	jr	.elias_pre
 .elias
 	add	STOR,STOR
 	jump	cs_ne,(LR2)
-	nop
-	jr	eq,.getbyte0
+	move	ELIAS,LR3
+	jr	eq,.getbyte
 	add	STOR,STOR
 .elias_pre
 	jr	ne,.elias
 	addc	VALUE,VALUE
-.getbyte0
 	move	ELIAS,LR3
-.getbyte1
-	move	BYTE_PRELOAD,STOR
-	addqt	#2,LR3
 .getbyte
+	addqt	#2,LR3
+	move	BYTE_PRELOAD,STOR
+.getbyte1
 	loadb	(SRC),BYTE_PRELOAD
 	shlq	#24,STOR
 	addqt	#1,SRC
 	bset	#23,STOR
 	jump	(LR3)
-.getbit
 	add	STOR,STOR
-	jump	ne,(LR3)
-	nop
-	jr	.getbyte
-	move	BYTE_PRELOAD,STOR
+
+	UNREG BYTE_PRELOAD,LR3,COPY_TMP,LR2
+	UNREG DATA_TMP,STOR,GETBYTE,ELIAS
+	UNREG LITERALS,OFFSET,NEW_OFF,COPY_MATCH,VALUE
