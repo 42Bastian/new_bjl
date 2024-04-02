6ec8ec6ac3814e2ed9fed712fe855b3368273dd4
42Bastian <bs42@monlynx.de>
2024-04-02 21:30:36 +0200

    unzx0: 136 bytes, 224ms (down from 236)

--- exp/depacker/unzx0.js
+++ exp/depacker/unzx0.js
@@ -13,99 +13,99 @@
 DST		REG 21
 SRC		REG 20
 
-LR3		REG 10
-LR2		REG 9
-STOR		REG 8
-GETBIT		REG 7
-ELIAS		REG 6
-LITERALS	REG 5
-
-OFFSET		REG 4
-COPY_MATCH	reg 3
-
-VALUE		REG 2
-tmp1		REG 1
-tmp0		REG 0
+LR3		REG 7
+TMP1		REG 7!
+LR2		REG 6
+TMP0		REG 6!
+STOR		REG 5
+GETBIT		REG 4
+LITERALS	REG 3
+OFFSET		REG 2
+COPY_MATCH	reg 1
+VALUE		REG 0
 
 unzx0::
 	movei	#.getbit,GETBIT
-	move	GETBIT,ELIAS
-	subq	#.getbit-.elias,ELIAS
 	moveq	#0,STOR
-	moveq	#1,OFFSET
-	moveq	#1,VALUE
-.literals
-	move	pc,LITERALS
-
 	move	pc,LR2
-	jump	(ELIAS)
-	addq	#6,LR2
+	moveq	#1,OFFSET
+.literals
+	jr	.to_elias0
+	move	LR2,LITERALS
+
 .copylit
-	loadb	(SRC),r0
+	loadb	(SRC),TMP1
 	addqt	#1,SRC
 	subq	#1,VALUE
-	storeb	r0,(DST)
-	jump	ne,(LR2)
+	storeb	TMP1,(DST)
+	jr	ne,.copylit
 	addqt	#1,DST
 
 __xx	move	pc,COPY_MATCH
-	move	pc,LR3
-	jump	(GETBIT)
-	addq	#6,LR3
-	addqt	#.copy_match-__xx,COPY_MATCH
+	jr	.to_getbit
+	move	COPY_MATCH,LR3
+
+	move	PC,LR2
 	jr	cs,.new_off
+	addqt	#.copy_match-__xx,COPY_MATCH
 	;; last offset
-	move	COPY_MATCH,LR2
-	jump	(ELIAS)
-	moveq	#1,VALUE
+
+.to_elias0:
+	jr	.to_elias
 
 .copy_match
-	move	DST,r1
-	sub	OFFSET,r1
+	move	DST,TMP1
+	sub	OFFSET,TMP1
 .copy_match_loop
-	loadb	(r1),r0
-	addqt	#1,r1
+	loadb	(TMP1),TMP0
+	addqt	#1,TMP1
 	subq	#1,VALUE
-	storeb	r0,(DST)
+	storeb	TMP0,(DST)
 	jr	ne,.copy_match_loop
 	addq	#1,DST
 
 	move	pc,LR3
+.to_getbit:
 	jump	(GETBIT)
 	addq	#6,LR3
-
-	jump	cc,(LITERALS)
 .new_off:
-	moveq	#1,VALUE
+	jr	cc,.to_elias
+	move	LITERALS,LR2
+
 	move	pc,LR2
-	jump	(ELIAS)
-	addq	#6,LR2
+.to_elias
+	moveq	#1,VALUE
+	jr	.elias
+	addq	#8,LR2
 __x0
 	move	VALUE,OFFSET
-	shrq	#8,VALUE
-	loadb	(SRC),r1
+	btst	#8,VALUE
+	loadb	(SRC),TMP1
 	jump	ne,(LR)
 	shlq	#7,OFFSET
 	addqt	#1,SRC
 	moveq	#1,VALUE
-	shrq	#1,r1
+	shrq	#1,TMP1
 	addqt	#__x1-__x0,LR2
 	jr	cc,.elias_pre
-	sub	r1,OFFSET
+	sub	TMP1,OFFSET
 __x1	jump	(COPY_MATCH)
 	addq	#1,VALUE
+
 
 .elias0
 	addc	VALUE,VALUE
 .elias
+	add	STOR,STOR
 	move	pc,LR3
-	jump	(GETBIT)
+	jr	eq,.getbyte
 	addqt	#6,LR3
 
 	jump	cs,(LR2)
+	nop
 .elias_pre
-	move	ELIAS,LR3
-	subqt	#2,LR3
+	move	PC,LR3
+	subq	#14,LR3
 
 .getbit
 	add	STOR,STOR
