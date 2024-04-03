d3e5d1c95a0261e18a3fb9c02b0ee688a0be33eb
42Bastian <bs42@monlynx.de>
2024-04-03 10:21:28 +0200

    unlz4: down to 94b

--- exp/depacker/unlz4.js
+++ exp/depacker/unlz4.js
@@ -14,16 +14,21 @@
 ;;; r12       : $ff
 ;;; r13       : end of packed data
 
-depack_lz4::
-	move	R20,R13
-	move	r20,r12
-	add	R0,R13		; packed buffer end
-	sat8	r12		; r12 = $ff
+DST		reg 21
+SRC		reg 20
+EOD		reg 13
+_FF		reg 12
+TOKENLOOP 	reg 11
 
-	loadb	(R20),R0
+depack_lz4::
+	move	SRC,EOD
+	move	SRC,_FF
+	add	R0,EOD		; packed buffer end
+	sat8	_FF		; _FF = $ff
 .dpklz4_tokenLoop:
-	move	pc,r11
-	addqt	#1,R20
+	move	pc,TOKENLOOP
+	loadb	(SRC),R0
+	addqt	#1,SRC
 	moveq	#15,r1
 	and	r0,r1
 	shrq	#4,R0
@@ -31,51 +36,48 @@ depack_lz4::
 	cmpq	#15,R0
 .dpklz4_readLen1:
 	jr	ne,.dpklz4_litcopy
-	loadb	(R20),R2
-	addqt	#1,R20
+	loadb	(SRC),R2
+	addqt	#1,SRC
 	add	R2,R0		; final len could be > 64KiB
 	jr	.dpklz4_readLen1:
-	cmp	R12,R2		; r2 = $ff ?
+	cmp	_FF,R2		; r2 = $ff ?
 
 .dpklz4_litcopy:
-	addqt	#1,R20
+	addqt	#1,SRC
 	subq	#1,R0
-	storeb	R2,(R21)
-	addqt	#1,R21
+	storeb	R2,(DST)
+	addqt	#1,DST
 	jr	ne,.dpklz4_litcopy
-.skip1
-	loadb	(R20),R2
+.skip1:
+	loadb	(SRC),R2
 
 	; end test is always done just after literals
-	cmp	R20,R13
+	cmp	SRC,EOD
+	addqt	#1,SRC
 	jump	eq,(r30)	; done? => return
 
-.dpklz4_lenOffset:
-	addqt	#1,R20
-	loadb	(R20),R0
-	addqt	#1,R20
+	loadb	(SRC),R0
 	shlq	#8,R0
 	add	R2,R0
 	neg	r0
+	add	DST,r0		; source = dest - offset
 	cmpq	#15,r1
+	jr	.dpklz4_copy0
 	addqt	#4,r1
-	jr	ne,.dpklz4_copy
-	add	r21,r0		; source = dest - offset
 
 .dpklz4_readLoop2:
-	loadb	(R20),R2
+	loadb	(SRC),R2
 	add	R2,R1		; final len could be > 64KiB
-	cmp	R12,R2		; r2 = $ff ?
+	cmp	_FF,R2		; r2 = $ff ?
+.dpklz4_copy0:
 	jr	eq,.dpklz4_readLoop2
-	addqt	#1,R20
+	addqt	#1,SRC
 
 .dpklz4_copy:
 	loadb	(R0),R2
-	addq	#1,R0
 	subq	#1,R1
-	storeb	R2,(R21)
-	jr	ne,.dpklz4_copy
-	addqt	#1,R21
-
-	jump	(R11)
-	loadb	(R20),R0
+	storeb	R2,(DST)
+	jump	eq,(TOKENLOOP)
+	addqt	#1,DST
+	jr	.dpklz4_copy
+	addqt	#1,R0
