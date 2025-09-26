32c91126d14f38e338ec54074d787240162469a4
42Bastian <bs42@monlynx.de>
2025-09-26 10:40:01 +0100

    prepare MAC mode support

--- lsp_lib/lsp_init.js
+++ lsp_lib/lsp_init.js
@@ -1,10 +1,10 @@
 ;;; -*-asm-*-
-; a0: music data (any mem)
-; a1: sound bank data (chip mem)
+; xa0: music data (any mem)
+; xa1: sound bank data (chip mem)
 
-a0	REG 10
-a1	REG 11
-a2	REG 12
+xa0	REG 10
+xa1	REG 11
+xa2	REG 12
 _a0	REG 13
 __a0	REG 16
 
@@ -22,56 +22,56 @@ OFF_DIV EQU 1
 	store	r0,(r14+3*OFF)
 	store	r0,(r14+4*OFF)
 
-	load	(r14),a0
-	load	(r14+1*OFF),a1
-	addq	#4,a0
-	addq	#4,a0		;skip ID
-	addq	#2,a0		;skip version
+	load	(r14),xa0
+	load	(r14+1*OFF),xa1
+	addq	#4,xa0
+	addq	#4,xa0		;skip ID
+	addq	#2,xa0		;skip version
 	movei	#LSPVars,r14
 
-	loadw	(a0),r6
-	addq	#2,a0
+	loadw	(xa0),r6
+	addq	#2,xa0
 	store	r6,(r14+(m_currentBpm-LSPVars)/OFF_DIV)	; default BPM
 	movei	#LSP_BPM_frequence_replay,r0
 	store	r6,(r0)
 
-	loadw	(a0),r6
-	addq	#2,a0
+	loadw	(xa0),r6
+	addq	#2,xa0
 	store	r6,(r14+(m_escCodeRewind-LSPVars)/OFF_DIV)
 
-	loadw	(a0),r6
-	addq	#2,a0
+	loadw	(xa0),r6
+	addq	#2,xa0
 	store	r6,(r14+(m_escCodeSetBpm-LSPVars)/OFF_DIV)
-//->load	(a0),r20; nb de ticks du module en tout = temps de replay ( /BPM)
-	addq	#4,a0
+//->load	(xa0),r20; nb de ticks du module en tout = temps de replay ( /BPM)
+	addq	#4,xa0
 
-	loadw	(a0),r0		; instrument count
-	addq	#2,a0
+	loadw	(xa0),r0		; instrument count
+	addq	#2,xa0
 
-	move	a0,a2
-	subq	#12,a2	; LSP data has -12 offset on instrument tab ( to win 2 cycles in fast player :) )
+	move	xa0,xa2
+	subq	#12,xa2	; LSP data has -12 offset on instrument tab ( to win 2 cycles in fast player :) )
 
 
-	store	a2,(r14+(m_lspInstruments-LSPVars)/OFF_DIV); instrument tab addr ( minus 4 )
+	store	xa2,(r14+(m_lspInstruments-LSPVars)/OFF_DIV); instrument tab addr ( minus 4 )
 
-	move	a0,__a0
+	move	xa0,__a0
 	addq	#2,__a0
 
 	move	pc,LOOP
 	addq	#4,LOOP
 .relocLoop:
-	loadw	(a0),r5		; pointeur sample
+	loadw	(xa0),r5		; pointeur sample
 	loadw	(__a0),r21
 	shlq	#16,r5
 	or	r21,r5
-	add	a1,r5		; passage de relatif en absolu
+	add	xa1,r5		; passage de relatif en absolu
 
 	move	r5,r21
 	storew	r21,(__a0)	; pointeur sample
 	shrq	#16,r21
-	storew	r21,(a0)
+	storew	r21,(xa0)
 
-	move	a0,_a0
+	move	xa0,_a0
 	addq	#4,_a0
 	loadw	(_a0),r6		; taille en words
 	shlq	#1,r6
@@ -81,7 +81,7 @@ OFF_DIV EQU 1
 	addq	#6,_a0
 
 	load	(_a0),r6		; pointeur sample repeat
-	add	a1,r6			; passage de relatif en absolu
+	add	xa1,r6			; passage de relatif en absolu
 	cmp	r5,r6	; corrige pointeur de repeat avant le debut
 	jr	pl,.ok_loop
 	nop
@@ -89,7 +89,7 @@ OFF_DIV EQU 1
 .ok_loop:
 	store	r6,(_a0)		; pointeur sample repeat
 
-	move	a0,_a0
+	move	xa0,_a0
 	addq	#10,_a0
 	loadw	(_a0),r6		; taille repeat en words
 	shlq	#1,r6
@@ -99,42 +99,42 @@ OFF_DIV EQU 1
 	subq	#1,r0
 	addqt	#12,__a0
 	jump	ne,(LOOP)
-	addqt	#12,a0
+	addqt	#12,xa0
 
-	loadw	(a0),r0		; codes count (+2)
-	addq	#2,a0
+	loadw	(xa0),r0		; codes count (+2)
+	addq	#2,xa0
 
-	store	a0,(r14+(m_codeTableAddr-LSPVars)/OFF_DIV) 	; code table
+	store	xa0,(r14+(m_codeTableAddr-LSPVars)/OFF_DIV) 	; code table
 	shlq	#1,r0
-	add	r0,a0
-	move	a0,__a0
+	add	r0,xa0
+	move	xa0,__a0
 	addq	#2,__a0
 
-	loadw	(a0),r0		; word stream size
+	loadw	(xa0),r0		; word stream size
 	loadw	(__a0),r21
 	shlq	#16,r0
 	or	r21,r0
-	addq	#4,a0
+	addq	#4,xa0
 	addq	#4,__a0
 
-	loadw	(a0),r1		; byte stream loop point
+	loadw	(xa0),r1		; byte stream loop point
 	loadw	(__a0),r21
 	shlq	#16,r1
 	or	r21,r1
-	addq	#4,a0
+	addq	#4,xa0
 	addq	#4,__a0
 
-	loadw	(a0),r2		; word stream loop point
+	loadw	(xa0),r2		; word stream loop point
 	loadw	(__a0),r21
 	shlq	#16,r2
 	or	r21,r2
-	addq	#4,a0
+	addq	#4,xa0
 
-	store	a0,(r14+(m_wordStream-LSPVars)/OFF_DIV)
-	move	a0,a1
-	add	r0,a1		; byte stream
-	store	a1,(r14)	;+m_byteStream-LSPVars)
-	add	r2,a0
-	add	r1,a1
-	store	a0,(r14+(m_wordStreamLoop-LSPVars)/OFF_DIV)
-	store	a1,(r14+(m_byteStreamLoop-LSPVars)/OFF_DIV)
+	store	xa0,(r14+(m_wordStream-LSPVars)/OFF_DIV)
+	move	xa0,xa1
+	add	r0,xa1		; byte stream
+	store	xa1,(r14)	;+m_byteStream-LSPVars)
+	add	r2,xa0
+	add	r1,xa1
+	store	xa0,(r14+(m_wordStreamLoop-LSPVars)/OFF_DIV)
+	store	xa1,(r14+(m_byteStreamLoop-LSPVars)/OFF_DIV)
